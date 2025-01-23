201a77cfeff8fea9dd88bf09d1b54de5eed7ad78
ultrasage-danz <olokodanielonline@gmail.com>
2025-01-23 19:32:41 +0100

    first commit

---
+++ agent/ai_researcher/demo.py
@@ -0,0 +1,48 @@
+"""Demo"""
+
+import os
+from dotenv import load_dotenv
+
+from fastapi import FastAPI
+import uvicorn
+from copilotkit.integrations.fastapi import add_fastapi_endpoint
+from copilotkit import CopilotKitRemoteEndpoint, LangGraphAgent
+from ai_researcher.agent import graph
+
+load_dotenv()
+
+app = FastAPI()
+sdk = CopilotKitRemoteEndpoint(
+    agents=[
+        LangGraphAgent(
+            name="ai_researcher",
+            description="Search agent.",
+            graph=graph,
+        )
+    ],
+)
+
+add_fastapi_endpoint(app, sdk, "/copilotkit")
+
+# add new route for health check
+@app.get("/health")
+def health():
+    """Health check."""
+    return {"status": "ok"}
+
+def main():
+    """Run the uvicorn server."""
+    port = int(os.getenv("PORT", "8000"))
+    uvicorn.run(
+        "ai_researcher.demo:app",
+        host="localhost",
+        port=port,
+        reload=True,
+        reload_dirs=(
+            ["."] +
+            (["../../../sdk-python/copilotkit"]
+             if os.path.exists("../../../sdk-python/copilotkit")
+             else []
+             )
+        )
+    )
